571b10e6c54760f3f468f6eaea53d32395541e00
xanddybr <xanddybr@gmail.com>
2025-02-21 23:30:07 -0300

    develop frontend landingpage

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
+const express = require('express')
+const { engine } = require('express-handlebars')
+const bodyPs = require('body-parser')
 
-functio plus() {
-return  1 + 1
-}
+const app = express()
+app.use('/bootstrap', express.static('./node_modules/bootstrap/dist'))
+app.use('/css', express.static('./css'))
+app.use(express.static('public'));
+
+
+app.use(bodyPs.urlencoded({ extended: false }))
+app.use(bodyPs.json())
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+
+app.get('/', (req, res) => {
+    res.render('form')
+})
+
+app.listen(3000, () => {
+    console.log('Server is running...')
+})  
+
